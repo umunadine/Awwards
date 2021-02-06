bd752ce95203c3c54d21f66674a41f4bb263fc7c
umunadine <umutesinad3@gmail.com>
2021-02-06 08:06:10 +0200

    adding urls

---
+++ awwardsApp/templates/index.js
@@ -0,0 +1,1 @@
+<h1>Welcomeee</h1>
---
+++ awwardsApp/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url,include
+from django.conf import settings
+from . import views
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$',views.index,name='index'),
+    url(r'^accounts/profile/', views.my_profile, name='my_profile'),
+    url(r'register/',views.register, name='register'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
