2980c30f5156695ff195521b5d9abfd4aab276cb
umunadine <umutesinad3@gmail.com>
2021-02-06 08:09:44 +0200

    import form

--- awwardsApp/forms.py
+++ awwardsApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Profile, Project
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import 
+from django.contrib.auth.forms import UserCreationForm
 
 
 class RegisterForm(UserCreationForm):
