0d34428ec44018096eade5fd86cde7d45f16b622
umunadine <umutesinad3@gmail.com>
2021-02-06 08:05:44 +0200

    changes

--- awwards/settings.py
+++ awwards/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awwardsApp',
     'bootstrap4',
+    'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 MEDIA_URL = '/media/'
