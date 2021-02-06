b5f53ce5ebad764995157561884e151c82612331
umunadine <umutesinad3@gmail.com>
2021-02-06 10:42:12 +0200

    adding urls

--- awwardsApp/urls.py
+++ awwardsApp/urls.py
@@ -8,6 +8,17 @@ urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^accounts/profile/', views.my_profile, name='my_profile'),
     url(r'register/',views.register, name='register'),
+    url(r'project/(\d+)',views.rate_project,name='rate-project'),
+    url(r'profile/(\d+)',views.profile,name='profile'),
+    url(r'my_profile',views.my_profile,name='my_profile'), 
+    url(r'^new/project$', views.new_project, name='new_project'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^api/profile', views.ProfileList.as_view(),name='api-profile'),
+    url(r'^api/project', views.ProjectList.as_view(),name='api-project'),
+    url(r'^api',views.api_page,name='api-page'),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
 
 ]
 if settings.DEBUG:
