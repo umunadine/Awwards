75ca31aa48e882d1288220b7337db71574d6983f
umunadine <umutesinad3@gmail.com>
2021-02-06 10:41:00 +0200

    adding urls

--- awwards/urls.py
+++ awwards/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('awwardsApp.urls')),
+     url(r'^logout/$', views.LogoutView.as_view(template_name='index.html'), {"next_page": '/'}),
 ]
